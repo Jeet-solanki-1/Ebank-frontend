1b2d261c4b4329fc8d3a2cb46e4a45e11798d466
jeet solanki <solankijeet285@gmail.com>
2025-10-08 18:08:46 +0530

    ngrok url changed for testing

--- config.js
+++ config.js
@@ -1,7 +1,5 @@
 // config.js
-const API_URL = 'https://d94f283f4052f4de872c54cd3341d83e.serveo.net'; // Replace with your actual backend URL
-
-
+const API_URL = 'https://a0a1e0bb1725.ngrok-free.app'; // Replace with your actual backend URL
 
 export default API_URL; // Export the API_URL so you can import it elsewhere
 
